7cc1586692de346b0bcfa1b834a43d1442e8c2d8
John Linatoc <johnlinatoc1227@gmail.com>
2021-02-08 21:32:31 -0600

    WIP: MonthlyBudget controller method rewrite.

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,20 @@ Rails
   .draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+    post '/auth', to: 'auth#create'
+    get '/current_user', to: 'auth#show'
+
     get '/profile', to: 'users#profile'
     get '/current_user/:id', to: 'users#show'
     post '/current_user', to: 'users#create'
     delete '/current_user', to: 'users#destroy'
+
     get '/users/:id/categories', to: 'categories#user_categories'
+
     get '/users/:id/expenses', to: 'expenses#user_expenses'
+    
     get '/users/:id/months', to: 'monthly_budgets#user_months'
-    post '/auth', to: 'auth#create'
-    get '/current_user', to: 'auth#show'
-    patch '/new_budget', to: 'monthly_budgets#newMonthBudget'
+    patch '/new_budget', to: 'monthly_budgets#editMonthlyIncome'
 
     resources :monthly_budgets, :categories, :expenses
     resources :users, except: [:new, :show, :patch]
